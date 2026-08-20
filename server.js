064cc38ee94ee897e392f41e4fdf50e1d6e66a93
anmiansu <anmiansu@gmail.com>
2026-08-20 20:47:23 -0300

    Verbo HTTP POST añadido

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import { pool } from "./db.js";
 
 const app = express(); // creamos una instancia del modulo express
 
+app.use(express.json()); //parseamos el json de la solicitud a un objeto javascript
+
 const PORT = 3000; // creamos la constante PORT para definir el puerto del servidor
 
 // definimos la ruta raiz del servidor de tipo GET, que devuelve un mensaje de bienvenida al usuario
@@ -13,6 +15,17 @@ app.get("/", (req, res) => {
 app.get("/api/platos", async (req, res) => {
     const { rows } = await pool.query("SELECT * FROM platos ORDER BY id")
     res.json(rows)
+})
+
+app.post("/api/platos", async (req, res) => {
+    const {nombre, precio} = req.body; //mediante destructuring obtenemos nombre y precio
+
+    if (!nombre || !precio) {
+        return res.status(400).json({error: "Bad Request - Datos Incorrectos"})
+    }
+
+    const { rows} = await pool.query("INSERT INTO platos (nombre, precio) VALUES ($1, $2) RETURNING *", [nombre, precio])
+    res.status(201).json(rows[0]) //retornamos la primera fila de la insercion 
 })
 
 // definimos la función para escuchar las solicitudes
